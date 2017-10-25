72f77b139b348dc21241874493bd44cf77cd89cb
shot4free <shotokan@ukr.net>
2017-10-25 20:14:55 +0200

    Added additional cams to reboot_camera.sh

--- reboot_camera.sh
+++ reboot_camera.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 exec 1> >(logger -s -t "$(basename $0) $1") 2>&1
 if [ "$1" == "eye01" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=cBNePvVgPxKUwsfa"
+    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
 elif [ "$1" == "eye02" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=I74acNKPI9Ctq3SF"
+    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
 elif [ "$1" == "eye03" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=SM1IcIS59RclJorj"
+    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
 else
     echo "Not recognized camera $1"
     exit 1
