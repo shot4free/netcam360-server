067450db5deadea7829db4350318838570a652b9
shot4free <shotokan@ukr.net>
2017-12-04 17:52:41 +0100

    Changed script reboot camera for using file camera.properties in /etc/camera

--- reboot_camera.sh
+++ reboot_camera.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 exec 1> >(logger -s -t "$(basename $0) $1") 2>&1
-if [ "$1" == "eye01" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
-elif [ "$1" == "eye02" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
-elif [ "$1" == "eye03" ]; then
-    curl "http://$1/reboot.cgi?loginuse=admin&loginpas=PASSWORD"
+if [ -f /etc/camera/${1}.properties ]; then
+  . /etc/camera/${1}.properties
+  curl "${URL}?loginuse=${USER}&loginpas=${PASS}"
 else
     echo "Not recognized camera $1"
-    exit 1
+    exit 1 ;;
 fi
-
